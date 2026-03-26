aed494d64232a59c02b06766d882fc9b44776025
Morloking <dyubenkosergei@yandex.ru>
2026-03-27 02:52:51 +0300

    Update rectangular_triangle.cpp

--- rectangular_triangle.cpp
+++ rectangular_triangle.cpp
@@ -2,6 +2,6 @@
 #include "my_exception.h"
 
 my_namespace::RectangularTriangle::RectangularTriangle(int l1, int l2, int l3, int c1, int c2, int c3) :
-	Triangle(l1, l2, l3, c1, c2, c3, "Прямоугольный треугольник") {
-	if (c3 != 90) throw MyException("угол C не равен 90");
+	Triangle(l1, l2, l3, c1, c2, c3, "РџСЂСЏРјРѕСѓРіРѕР»СЊРЅС‹Р№ С‚СЂРµСѓРіРѕР»СЊРЅРёРє") {
+	if (c3 != 90) throw MyException("СѓРіРѕР» C РЅРµ СЂР°РІРµРЅ 90");
 }
