635a880e4f4a7a890c9a6a5911ca1588550f91fa
Morloking <dyubenkosergei@yandex.ru>
2026-03-27 02:53:24 +0300

    Update rhomb.cpp

--- rhomb.cpp
+++ rhomb.cpp
@@ -2,7 +2,7 @@
 #include "my_exception.h"
 
 my_namespace::Rhomb::Rhomb(int l1, int l2, int l3, int l4, int c1, int c2, int c3, int c4) :
-	Quadrangle(l1, l2, l3, l4, c1, c2, c3, c4, "Ромб") {
-	if (l1 != l2 || l2 != l3 || l3 != l4) throw MyException("не все стороны равны");
-	if (c1 != c3 || c2 != c4) throw MyException("углы A,C и B,D попарно не равны");
+	Quadrangle(l1, l2, l3, l4, c1, c2, c3, c4, "Р РѕРјР±") {
+	if (l1 != l2 || l2 != l3 || l3 != l4) throw MyException("РЅРµ РІСЃРµ СЃС‚РѕСЂРѕРЅС‹ СЂР°РІРЅС‹");
+	if (c1 != c3 || c2 != c4) throw MyException("СѓРіР»С‹ A,C Рё B,D РїРѕРїР°СЂРЅРѕ РЅРµ СЂР°РІРЅС‹");
 }
